34f0e49d888f4be3fc5981c4de2ede270a8791e8
bedy-kharisma <88810292+bedy-kharisma@users.noreply.github.com>
2023-02-22 10:59:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -18,13 +18,14 @@ from google.oauth2 import service_account
 from gsheetsdb import connect
 import pyparsing
 import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 
 # Create a connection object.
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"],
     scopes=[
-        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive"
     ],
 )
 conn = connect(credentials=credentials)

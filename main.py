0dd2476e2028b082af66c22af6c36cee094b14a7
bedy-kharisma <88810292+bedy-kharisma@users.noreply.github.com>
2023-02-22 11:03:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -22,13 +22,19 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 
 # Create a connection object.
-credentials = service_account.Credentials.from_service_account_info(
+#credentials = service_account.Credentials.from_service_account_info(
+#    st.secrets["gcp_service_account"],
+#    scopes=[
+#        "https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive"
+#    ],
+#)
+#conn = connect(credentials=credentials)
+credentials = ServiceAccountCredentials.from_service_account_info(
     st.secrets["gcp_service_account"],
     scopes=[
-        "https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive"
-    ],
+            "https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive"
+        ],
 )
-conn = connect(credentials=credentials)
 client=gspread.authorize(credentials)
 
 def run_query(query):

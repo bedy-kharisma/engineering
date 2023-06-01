abfd1cc76d063d63adce8b9a2935f5d940b5aeaa
bedy-kharisma <88810292+bedy-kharisma@users.noreply.github.com>
2023-06-01 12:04:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from github import Github, UnknownObjectException
 from google.oauth2 import service_account
 import pyparsing
 import gspread
+#for cluster & mtbf
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans, MiniBatchKMeans
 from sklearn.metrics import silhouette_score
@@ -31,6 +32,15 @@ import matplotlib.gridspec as gridspec
 from docx import Document
 from docx.shared import Inches
 import warnings
+#for chat
+from haystack import Pipeline
+from haystack.nodes import TextConverter, PreProcessor
+from haystack.document_stores import FAISSDocumentStore, InMemoryDocumentStore
+from haystack.pipelines.standard_pipelines import TextIndexingPipeline
+from sentence_transformers import SentenceTransformer
+from haystack.nodes import EmbeddingRetriever, FARMReader
+from haystack.pipelines import ExtractiveQAPipeline
+from haystack.nodes import PromptNode, PromptTemplate
 
 warnings.filterwarnings("ignore")
 #Create a word doc
@@ -1384,6 +1394,78 @@ def MTBF():
         st.subheader("Or if you already have filled delivery data and cluster data, upload to the following")
         mtbf_clc(doc)
 
+def chat():
+    # Load the dataframe from a pickle file
+    df = pd.read_pickle('./standards.pkl')
+    df['num_chars'] = df['text'].apply(lambda x: len(x))
+    df = df[df['num_chars'] != 0]
+    df = df[['name', 'url', 'text']]
+    selected_columns = ['name', 'text']
+    df = df[selected_columns]
+    keyword = st.text_input('Choose topic', "running dynamic")
+    filtered_std = df[df['text'].str.contains(keyword, flags=re.IGNORECASE)]
+    # Create a folder to store the text files
+    folder = 'text_files'
+    doc_dir = 'text_files'
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+    # Replace file extensions in file names
+    filtered_std['name'] = filtered_std['name'].str.replace('.pdf', '.txt')
+    # Loop through each row of the dataframe
+    for index, row in filtered_std.iterrows():
+        # Get the file name and text for this row
+        file_name = row['name']
+        text = row['text']
+        # Create a new text file with the given file name and write the text to it
+        file_path = os.path.join(folder, file_name)
+        with open(file_path, 'w', errors='ignore') as f:
+            f.write(text)
+    #defining pipeline
+    document_store = InMemoryDocumentStore()
+    indexing_pipeline = Pipeline()
+    text_converter = TextConverter()
+    preprocessor = PreProcessor(
+        clean_whitespace=True,
+        clean_header_footer=True,
+        clean_empty_lines=True,
+        split_by="word",
+        split_length=1000,
+        split_overlap=20,
+        split_respect_sentence_boundary=True,
+    )
+    indexing_pipeline.add_node(component=text_converter, name="TextConverter", inputs=["File"])
+    indexing_pipeline.add_node(component=preprocessor, name="PreProcessor", inputs=["TextConverter"])
+    indexing_pipeline.add_node(component=document_store, name="DocumentStore", inputs=["PreProcessor"])
+    #reads all related files
+    files_to_index = [doc_dir + "/" + f for f in os.listdir(doc_dir)]
+    indexing_pipeline.run_batch(file_paths=files_to_index)
+    retriever = EmbeddingRetriever(
+        document_store=document_store, embedding_model="sentence-transformers/multi-qa-mpnet-base-dot-v1"
+    )
+    document_store.update_embeddings(retriever)
+    reader = FARMReader(model_name_or_path="bert-large-uncased-whole-word-masking-finetuned-squad", use_gpu=True)
+    pipe = ExtractiveQAPipeline(reader, retriever)
+    #QnA
+    k = 5
+    query  = st.text_input('Input query:', "vehicle at what speed that must perform dynamic performance test?")
+    prediction = pipe.run(
+        query=query,
+        params={
+            "Retriever": {"top_k": k * 3},
+            "Reader": {"top_k": k * 2}
+        }
+    )
+    answer_contexts = []
+    for i in range(k):
+        answer_context = prediction['answers'][i].context
+        answer_context = answer_context.replace('\n', ' ')  # Remove line feeds
+        answer_contexts.append(answer_context)
+    joined_contexts = ' '.join(answer_contexts)
+
+    prompt_node = PromptNode(model_name_or_path="google/flan-t5-base", use_gpu=True)
+    prompt_text = "Consider you are a rolling stock consultant provided with this query: {query} provide answer from the following context: {contexts}. Answer:"
+    output = prompt_node.prompt(prompt_template=prompt_text, query=query, contexts=joined_contexts)
+    st.write(output[0])
 
 page_names_to_funcs = {
     "Product Breakdown Structure": system_requirement,

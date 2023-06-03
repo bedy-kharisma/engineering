72157a96be154877e443c215e8c9b583149ed127
bedy-kharisma <88810292+bedy-kharisma@users.noreply.github.com>
2023-06-03 09:59:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -265,7 +265,8 @@ def Standards():
 
         # Display the DataFrame
         gd=GridOptionsBuilder.from_dataframe(standards_df)
-        gd.configure_column("id", headerName="id", cellRenderer=markdown('''function(params) {return '<a href="https://drive.google.com/file/d/' + params.value + '/view" target="_blank">' + params.value + '</a>';}'''))
+        #gd.configure_column("id", headerName="id", cellRenderer=markdown('''function(params) {return '<a href="https://drive.google.com/file/d/' + params.value + '/view" target="_blank">' + params.value + '</a>';}'''))
+        gd.configure_column("id", headerName="id", cellRenderer=markdown('''function(params) { return '<a href="https://drive.google.com/file/d/' + params.value + '/view" target="_blank">' + params.value + '</a>'; }'''))
 
         #gd.configure_column("id", headerName="id", cellRenderer=markdown('''function(params) {return '<a href="https://drive.google.com/file/d/' + params.value + '/view" target="_blank">' + params.value + '</a>'}'''),
         #                width=300)

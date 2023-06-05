c79b6516ebb95ee268279643438fef1907202586
bedy-kharisma <88810292+bedy-kharisma@users.noreply.github.com>
2023-06-05 13:10:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -46,6 +46,7 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
+from openai import OpenAIEmbeddings
 
 warnings.filterwarnings("ignore")
 #Create a word doc

5e00b421f996a5fec2af685f851fd6ccd2b2777b
bedy-kharisma <88810292+bedy-kharisma@users.noreply.github.com>
2023-06-05 09:33:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1312,8 +1312,7 @@ def chat():
     selected_columns = ['name', 'text']
     df = df[selected_columns]
     keyword = st.text_input("Insert Topic", "running dynamic")
-    user_question = st.text_input("Ask a question about your documents:","vehicle at what speed that must perform dynamic performance test?"
-)
+    user_question = st.text_input("Ask a question about your documents:","vehicle at what speed that must perform dynamic performance test?")
     if st.button("Process"):
         filtered_std = df[df['text'].str.contains(keyword, flags=re.IGNORECASE)]
         column_values = filtered_std['text'].astype(str).values
@@ -1332,11 +1331,7 @@ def chat():
         output = requests.post(API_URL, headers=headers, json=({"inputs": {"source_sentence": user_question,"sentences": chunks},}))
         scores=output.json()
         combined_data = list(zip(chunks.values(), scores))
-	data = {
-	    "Chunks": [chunk for chunk, _ in combined_data],
-	    "Score": [score for _, score in combined_data]
-	}
-
+	data = {"Chunks": [chunk for chunk, _ in combined_data],"Score": [score for _, score in combined_data]}
 	df = pd.DataFrame(data)
 	st.write(df)
 

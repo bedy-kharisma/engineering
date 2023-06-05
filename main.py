f75887f221a8d54aef25ee181902b60f1ad30e9f
bedy-kharisma <88810292+bedy-kharisma@users.noreply.github.com>
2023-06-05 10:57:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1365,14 +1365,14 @@ def chat():
 	pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
 	if st.button("Process"):
     		with st.spinner("Processing"):
-		# get pdf text
-		raw_text = get_pdf_text(pdf_docs)
-		# get the text chunks
-		text_chunks = get_text_chunks(raw_text)
-		# create vector store
-		vectorstore = get_vectorstore(text_chunks)
-		# create conversation chain
-		st.session_state.conversation = get_conversation_chain(vectorstore)
+			# get pdf text
+			raw_text = get_pdf_text(pdf_docs)
+			# get the text chunks
+			text_chunks = get_text_chunks(raw_text)
+			# create vector store
+			vectorstore = get_vectorstore(text_chunks)
+			# create conversation chain
+			st.session_state.conversation = get_conversation_chain(vectorstore)
 	st.write(css, unsafe_allow_html=True)
 	if "conversation" not in st.session_state:
 		st.session_state.conversation = None

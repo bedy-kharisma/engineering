b03bc80b6bcc77fbda57c112e804a25fc44c2d4c
bedy-kharisma <88810292+bedy-kharisma@users.noreply.github.com>
2023-06-07 08:52:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1371,7 +1371,7 @@ def chat():
 		qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=OPENAI_API_KEY), chain_type="map_rerank", retriever=docsearch.as_retriever(),return_source_documents=True)
 		result = qa({"query": query})
 		st.write("Answer :")
-	 	st.write(result["result"])
+		st.write(result["result"])
 		st.markdown("---")
 		st.write("Sources :")
 		source_documents = [doc.page_content for doc in result["source_documents"]]

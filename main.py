140a4998e071763deb69938cae1c1ae110a13e5f
bedy-kharisma <88810292+bedy-kharisma@users.noreply.github.com>
2023-06-12 09:10:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1397,8 +1397,6 @@ page_names_to_funcs = {
 from streamlit_option_menu import option_menu
 
 with st.sidebar:
-    selected_page = option_menu("Main Menu", list(page_names_to_funcs.keys()) ) 
+    selected_page = option_menu("Main Menu", list(page_names_to_funcs.keys()), "other Menu", list(page_names_to_funcs.keys()) ) 
         #icons=['house', 'gear'], menu_icon="cast", default_index=1)
 page_names_to_funcs[selected_page]()
-with st.sidebar:
-    selected_page = option_menu("Main Menu", list(page_names_to_funcs.keys()) ) 
